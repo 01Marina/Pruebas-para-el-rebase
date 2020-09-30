e02c406cafa2de3b9e685e04be19b7b528113ba3
Cecilia Carlon <ceciliacarlon2@gmail.com>
2020-09-29 21:28:07 -0300

    Nuevo mensaje de despedida.

--- router.php
+++ router.php
@@ -1,7 +1,11 @@
 <?php
     echo "Buenas tardes";
 <<<<<<< HEAD
+<<<<<<< HEAD
     echo "nos vemos";
 =======
 >>>>>>> b0962a7... Nuevo mensaje de bienvenida
+=======
+    echo "nos vemos";
+>>>>>>> 9bd5eff... Nuevo mensaje de despedida
 ?>
