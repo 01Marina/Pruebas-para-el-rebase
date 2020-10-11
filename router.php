d6ba3fde9cbec7dece32c6a36e3693abb4b622c1
Cecilia Carlon <ceciliacarlon2@gmail.com>
2020-10-11 17:33:03 -0300

    Agrego un if a el router.

--- router.php
+++ router.php
@@ -3,9 +3,16 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
     echo "nos vemos";
+<<<<<<< HEAD
 =======
 >>>>>>> b0962a7... Nuevo mensaje de bienvenida
 =======
     echo "nos vemos";
 >>>>>>> 9bd5eff... Nuevo mensaje de despedida
 ?>
+=======
+    if (let suma=10 || let resta=4){
+      echo "Se esta sumando y restando";
+    }
+?>
+>>>>>>> acb18c2... Agrego un if a el router
